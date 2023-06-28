4222b766ad7168ab216ddb7bdde09d89d6e4ac25
KladeRe <klaus.rehnberg@gmail.com>
2023-06-28 20:17:13 +0300

    Added some remaps and zen mode

--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -149,6 +149,11 @@ _G.packer_plugins = {
     loaded = true,
     path = "/home/klade/.local/share/nvim/site/pack/packer/start/undotree",
     url = "https://github.com/mbbill/undotree"
+  },
+  ["zen-mode.nvim"] = {
+    loaded = true,
+    path = "/home/klade/.local/share/nvim/site/pack/packer/start/zen-mode.nvim",
+    url = "https://github.com/folke/zen-mode.nvim"
   }
 }
 
